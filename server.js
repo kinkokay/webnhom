107d6e17c2f2f27d8f916cdf4a17969fa4c3598e
kinkokay <kienweed@gmail.com>
2025-12-30 13:17:19 +0700

    Fix render

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./ooia-backend/db');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors()); // Cho phép mọi nguồn truy cập (Dev mode)
@@ -104,9 +104,9 @@ app.post('/api/orders', async (req, res) => {
     } finally {
         connection.release();
     }
-});
+}); 
 
 // Chạy server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
