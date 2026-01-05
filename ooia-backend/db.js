fe02efea4e39536846ad73f059b4572c2affa3f6
kinkokay <kienweed@gmail.com>
2026-01-05 15:52:13 +0700

    fix password

--- ooia-backend/db.js
+++ ooia-backend/db.js
@@ -5,10 +5,10 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, 
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     ssl: {
-        rejectUnauthorized: false // Dòng này quan trọng để kết nối Aiven nhanh
+        rejectUnauthorized: false
     },
     waitForConnections: true,
     connectionLimit: 10,
